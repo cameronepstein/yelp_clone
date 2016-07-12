ccd2695863dc24085f1cfc2c316e203963f85ac3
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-07-12 11:48:41 +0100

    user can add restaurants to database

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,22 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    # raise "pretty little lemon"
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
+
 feature 'restaurants' do
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+    scenario 'it should create a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
+
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'should display a restaurant once added' do
+      visit '/restaurants'
+      click_link('Add a restaurant')
+      fill_in('Name', with: 'Cams Chicken Hut')
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Cams Chicken Hut'
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
